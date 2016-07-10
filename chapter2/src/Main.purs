7139589f2481355d05886256475f58f6d5989aba
Matt Audesse <matt@mattaudesse.com>
2016-07-10 09:45:35 -0400

    Experiments with chapter2/src/Main.purs

--- chapter2/src/Main.purs
+++ chapter2/src/Main.purs
@@ -1,10 +1,16 @@
 module Main where
 
-import Prelude ((+), (*))
-import Control.Monad.Eff.Console (logShow)
-import Math (sqrt)
+import Prelude                   ((+), (*), Unit, bind)
+import Control.Monad.Eff         (Eff)
+import Control.Monad.Eff.Console (CONSOLE, logShow)
+import Math                      (sqrt)
 
 diagonal :: Number -> Number -> Number
 diagonal w h = sqrt (w * w + h * h)
 
-main = logShow (diagonal 3.0 4.0)
+main :: forall t4. Eff ( console :: CONSOLE | t4 ) Unit
+main = do
+    logShow (diagonal 3.0   4.0)
+    logShow (diagonal 2.0   6.0)
+    logShow (diagonal 100.1 4.3)
+    logShow (diagonal 8.34  0.9)
